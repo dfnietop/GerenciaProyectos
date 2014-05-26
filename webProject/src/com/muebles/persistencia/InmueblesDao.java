708ef24c4c03d7a501209a9b8bbf538b88f5cb85
jorgeandresrodriguez <big9212@hotmail.com>
2014-05-26 02:36:00 -0500

    Update InmueblesDao.java

--- webProject/src/com/muebles/persistencia/InmueblesDao.java
+++ webProject/src/com/muebles/persistencia/InmueblesDao.java
@@ -1,10 +1,41 @@
 package com.muebles.persistencia;
 
-public class InmueblesDao {
+import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+
+
+public class InmueblesDao {
+	Inmuebles registro=new Inmuebles();
+	ArrayList lista = new ArrayList();
 	public void crearInmueble(Inmuebles Inm, Conexion conexion) {
+		///Conexion conexion1 = new Conexion();
 		try {
-			conexion.getInstance().crearConexion();
+			
+			System.out.println(registro.getINMNOM());
+			Connection con = conexion.getInstance().crearConexion();
+			Statement st = con.createStatement();
+			//String sql="INSERT INTO  inmueble (nombre,direccion,telefono,estado,negocio,id) "
+				//	+ "VALUES('hola','hola','hola','hola','hola',1);"; 
+			String sql ="INSERT INTO  inmueble (idin,nombre,direccion,telefono,estado,negocio,comentarios,id)"+" VALUES(3,"+"'"+
+			registro.getINMNOM()+"',"
+				+ "'"+registro.getINMDIR()+"',"
+							+ "'"+registro.getINMTEL()+"',"
+							+ "'"+registro.getINMVENOARR()
+							+"','"+registro.getINMNEGOC()+"'"
+							+",'"+registro.getINCCOMMENTS()+"'"
+							+",1);";
+			//PreparedStatement pstmt = con.prepareStatement(sql);
+			System.out.println(sql);
+			st.executeUpdate(sql);
+			System.out.println(st.execute(sql));
+			//ResultSet resultado = comando.executeQuery(sql);
+//System.out.println(pstmt);
+		//INSERT INTO  inmueble (nombre,direccion,telefono,estado,negocio,id) 
+/// VALUES('hola','hola','hola','hola','hola',1);
 			// TODO: REALIZAR LA INSERCION EN LA BASE DE DATOS, ES DECIR USTED
 			// DEBE PONER LA ESTRUCTURA DE INSERCION DE MYSQL QUE NOS BRINDA
 			// JAVA, COMO LO HACE BUSQUE EN INTERNET COMO CREAR UN METODO DE
@@ -17,10 +48,35 @@ public class InmueblesDao {
 		}
 
 	}
-	
+	/**
 	public void consultarInmueble(Inmuebles Inm, Conexion conexion) {
 		try {
+			String a;
+			String b;
+			String c;
+			String d;
+			String f;
+			
 			conexion.getInstance().crearConexion();
+			//Statement comando = conexion.createStatement();
+			String sql = "SELECT id,nombre,direccion,telefono,estado FROM inmuebles WHERE id="
+					+ Inm.getId();
+			ResultSet resultado = comando.executeQuery(sql);
+			while (resultado.next()) {
+				
+				a = resultado.getString("id");
+				b = resultado.getString("nombre");
+				c = resultado.getString("direccion");
+				d = resultado.getString("telefono");
+				f = resultado.getString("estado");
+
+				lista.add(a);
+				lista.add(b);
+				lista.add(c);
+				lista.add(d);
+				lista.add(f);
+			
+			}
 			// TODO: REALIZAR LA INSERCION EN LA BASE DE DATOS, ES DECIR USTED
 			// DEBE PONER LA ESTRUCTURA DE INSERCION DE MYSQL QUE NOS BRINDA
 			// JAVA, COMO LO HACE BUSQUE EN INTERNET COMO CREAR UN METODO DE
@@ -33,7 +89,7 @@ public class InmueblesDao {
 		}
 
 	}
-	
+*/	
 
 
 }
